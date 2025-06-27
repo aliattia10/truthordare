d9b3a0a820899260bfde255bfed88ae25bd4211a
Ali attia <attiaali853@gmail.com>
2025-06-28 01:23:50 +0200

    chore: update SOCKET_SERVER_URL to Render backend

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -7,12 +7,12 @@ const isProduction = import.meta.env.PROD;
 // TODO: Deploy server to Render and update this URL
 export const SOCKET_SERVER_URL = isDevelopment 
   ? 'http://localhost:3001' 
-  : 'https://couple-dare-date-server.onrender.com'; // TODO: Update after server deployment
+  : 'https://truthordare-vouh.onrender.com';
 
 // API endpoints
 export const API_BASE_URL = isDevelopment 
   ? 'http://localhost:3001' 
-  : 'https://couple-dare-date-server.onrender.com';
+  : 'https://truthordare-vouh.onrender.com';
 
 // Environment info
 export const ENV = {
